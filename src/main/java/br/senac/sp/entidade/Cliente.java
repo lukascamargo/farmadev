651be39b794060b0810e25c25c41c56cce3e6e7b
Caique.Moreira <caique_moreira7@hotmail.com>
2020-05-01 21:44:48 -0300

    criação cadastro Cliente

---
+++ src/main/java/br/senac/sp/entidade/Cliente.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.sp.entidade;
+
+/**
+ *
+ * @author tiago.bscarton
+ */
+public class Cliente {
+
+    private String nome;
+    private String email;
+    private int telefone;
+    private int cpf;
+    private String endereco;
+    private String genero;
+    private String estadoCivil;
+
+    public Cliente(String nome, String email, int telefone, int cpf, String endereco, String genero, String estadoCivil) {
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.cpf = cpf;
+        this.endereco = endereco;
+        this.genero = genero;
+        this.estadoCivil = estadoCivil;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getEstadoCivil() {
+        return estadoCivil;
+    }
+
+    public void setEstadoCivil(String estadoCivil) {
+        this.estadoCivil = estadoCivil;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
